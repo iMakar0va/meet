4286baa96509225b5ec86acfaf3b16ddc4ba36c0
makar0vaIrina <imakarova.spb@gmail.com>
2025-02-13 10:21:01 +0300

    update conn.php

--- php/conn.php
+++ php/conn.php
@@ -1,10 +1,26 @@
 <?php
-$host = 'localhost';
+// $host = 'localhost';
+// $user = 'postgres';
+// $password = '123';
+// $dbname = 'meet2';
+
+// $conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+// if (!$conn) {
+//     die("Ошибка подключения к базе данных: " . pg_last_error());
+// }
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Подключение к удаленной БД
+$host = '188.241.218.41';  // IP или домен сервера PostgreSQL
+$port = '5432';
 $user = 'postgres';
 $password = '123';
 $dbname = 'meet2';
 
-$conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
 if (!$conn) {
-    die("Ошибка подключения к базе данных: " . pg_last_error());
+    die(json_encode(["error" => "Ошибка подключения: " . pg_last_error()]));
 }
+pg_close($conn);
