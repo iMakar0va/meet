84e8ddaeedcfd2d540f3b9ea3688ac8378a137e7
makar0vaIrina <imakarova.spb@gmail.com>
2025-02-13 14:18:54 +0300

    обновление

--- php/card.php
+++ php/card.php
@@ -1,4 +1,5 @@
 <?php
+// Массив для месяцев
 $months = [
     1 => 'января',
     'февраля',
@@ -13,14 +14,19 @@ $months = [
     'ноября',
     'декабря'
 ];
+
+// Предполагаем, что $row содержит данные из базы, полученные с использованием PDO
 $dateParts = explode('-', $row['event_date']);
 $formattedDate = intval($dateParts[2]) . ' ' . $months[intval($dateParts[1])];
 
+// Обработка изображения (если оно существует)
 $imageSrc = !empty($row["image"])
-    ? "data:image/jpeg;base64," . base64_encode(pg_unescape_bytea($row["image"]))
-    : "img/profile.jpg"; ?>
+    ? "data:image/jpeg;base64," . base64_encode($row["image"]) // Здесь уже не нужно использовать pg_unescape_bytea, так как с PDO это не нужно
+    : "img/profile.jpg";
+?>
+
 <div class="card">
-    <div class="card__img" style="background-image: url(<?= $imageSrc ?>)"></div>
+    <div class="card__img" style="background-image: url(<?= htmlspecialchars($imageSrc) ?>)"></div>
     <div class="card__content">
         <div class="card__type"><?= htmlspecialchars($row["type"]) ?></div>
         <div class="card__date"><?= htmlspecialchars($formattedDate) ?></div>
