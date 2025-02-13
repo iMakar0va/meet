6e3af542c01ff691338cb452169a2f732cfd9c94
makar0vaIrina <imakarova.spb@gmail.com>
2025-02-13 13:52:48 +0300

    update conn.php final

--- php/conn.php
+++ php/conn.php
@@ -8,28 +8,15 @@
 // if (!$conn) {
 //     die("Ошибка подключения к базе данных: " . pg_last_error());
 // }
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+$host = 'roundhouse.proxy.rlwy.net';  // Хост от Railway
+$port = '55214';                     // Порт от Railway
+$user = 'postgres';                  // Ваш PostgreSQL пользователь
+$password = 'KalODFtzEMVpNfunTSziOygJjQpyhGzv';  // Ваш пароль от Railway
+$dbname = 'railway';                 // Имя базы данных, указанное на Railway
 
-// Данные для подключения
-$host = '188.241.218.41';
-$port = '5432';
-$user = 'postgres';
-$password = '123';
-$dbname = 'meet2';
-
-// Подключение с проверкой
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password options='--client_encoding=UTF8'");
-
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 if (!$conn) {
-    die(json_encode(["error" => "Ошибка подключения: " . pg_last_error()]));
+    die("Ошибка подключения к базе данных: " . pg_last_error());
 }
 
-// Проверка подключения
-$result = pg_query($conn, "SELECT 1");
-if (!$result) {
-    die(json_encode(["error" => "База недоступна: " . pg_last_error()]));
-}
-
-// Закрываем соединение
-pg_close($conn);
+// Дополнительные действия с базой данных
