6dfc2b921ed3ad6c8c206e9c688a0e38992e4bf9
makar0vaIrina <imakarova.spb@gmail.com>
2025-02-13 14:04:16 +0300

    обновление

--- php/conn.php
+++ php/conn.php
@@ -8,15 +8,19 @@
 // if (!$conn) {
 //     die("Ошибка подключения к базе данных: " . pg_last_error());
 // }
-$host = 'postgres.railway.internal';  // Хост от Railway
-$port = '5432';                     // Порт от Railway
-$user = 'postgres';                  // Ваш PostgreSQL пользователь
-$password = 'KalODFtzEMVpNfunTSziOygJjQpyhGzv';  // Ваш пароль от Railway
-$dbname = 'railway';                 // Имя базы данных, указанное на Railway
+$host = 'roundhouse.proxy.rlwy.net';  // Хост для внешнего подключения
+$port = '55214';                      // Порт для внешнего подключения
+$user = 'postgres';                    // Имя пользователя
+$password = 'KalODFtzEMVpNfunTSziOygJjQpyhGzv';  // Пароль
+$dbname = 'railway';                  // Имя базы данных
 
+// Строка подключения
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
 if (!$conn) {
     die("Ошибка подключения к базе данных: " . pg_last_error());
 }
 
-// Дополнительные действия с базой данных
+echo "Подключение успешно!";
+?>
+
